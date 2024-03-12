2853e636a8841a1717ac0732487e7696480f2f5e
GANIDU2413 <ganidusahan@gmail.com>
2024-03-12 17:42:48 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './layout/NavBar';
 import Home from './pages/Home';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import AddUser from './users/AddUser';
 
 function App() {
   return (
     <div className="App">
-       <NavBar/>
-       <Home/>
+      <Router>
+        <NavBar/>
+        <Routes>
+          <Route excat path='/' element={<Home/>}></Route>
+          <Route excat path='/adduser' element={<AddUser/>}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
