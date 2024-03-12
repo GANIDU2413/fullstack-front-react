e8ea4a86f4439ed4745945e3ab1339cb1e3d32be
GANIDU2413 <ganidusahan@gmail.com>
2024-03-12 12:42:28 +0530

    changes

---
+++ src/pages/Home.js
@@ -0,0 +1,54 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+
+export default function Home() {
+    const [users,setUser]=useState([]);
+
+    useEffect(()=> {
+        loadUsers();  
+    },[]);
+
+    const loadUsers=async()=>{
+        const result = await axios.get("http://localhost:8080/users");
+        setUser(result.data);
+    }
+
+  return (
+    <div className='container'>
+        <div className='py-4'>
+        <table className="table border shadow">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Name</th>
+      <th scope="col">User Name</th>
+      <th scope="col">E-mail</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+
+    {
+        users.map((user,index)=>(
+            <tr>
+                <th scope="row" key={index}>{index+1}</th>
+                <td>{user.name}</td>
+                <td>{user.username}</td>
+                <td>{user.email}</td>
+                <td>
+                    <button className='btn btn-primary mx-3 btn-sm'>View</button>
+                    <button className='btn btn-outline-primary mx-2 btn-sm'>Edit</button>
+                    <button className='btn btn-danger mx-2 btn-sm'>Delete</button>
+
+
+                </td>
+            </tr>
+        ))
+    }
+    
+  </tbody>
+</table>
+        </div>
+    </div>
+  )
+}
